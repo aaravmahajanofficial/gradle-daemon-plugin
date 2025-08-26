bbedbd66c00b49c8980a58349a61831a9176c53c
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-08-26 23:42:22 +0530

    feat: use convention-plugin

--- src/test/java/org/gradlecommunity/jenkins/daemon/GradleDaemonKillingVetoTest.java
+++ src/test/java/org/gradlecommunity/jenkins/daemon/GradleDaemonKillingVetoTest.java
@@ -1,23 +1,23 @@
 package org.gradlecommunity.jenkins.daemon;
 
+import static org.junit.Assert.*;
+
 import com.google.common.collect.Lists;
 import hudson.EnvVars;
 import hudson.util.ProcessKillingVeto;
 import hudson.util.ProcessTree;
 import hudson.util.ProcessTreeRemoting;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.util.List;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class GradleDaemonKillingVetoTest {
     private GradleDaemonKillingVeto killer = new GradleDaemonKillingVeto();
 
     @Test
     public void testSunnyDay() {
-        ProcessKillingVeto.VetoCause veto = killer.vetoProcessKilling(test("org.gradle.launcher.daemon.bootstrap.GradleDaemon"));
+        ProcessKillingVeto.VetoCause veto =
+                killer.vetoProcessKilling(test("org.gradle.launcher.daemon.bootstrap.GradleDaemon"));
         assertNotNull(veto);
     }
 
@@ -30,12 +30,10 @@ public class GradleDaemonKillingVetoTest {
     private ProcessTreeRemoting.IOSProcess test(String name) {
         return new ProcessTreeRemoting.IOSProcess() {
             @Override
-            public void killRecursively() throws InterruptedException {
-            }
+            public void killRecursively() throws InterruptedException {}
 
             @Override
-            public void kill() throws InterruptedException {
-            }
+            public void kill() throws InterruptedException {}
 
             @Override
             public int getPid() {
@@ -55,15 +53,15 @@ public class GradleDaemonKillingVetoTest {
             @Override
             public List<String> getArguments() {
                 return Lists.newArrayList(
-                        "/java/bin/java"
-                        ,"-Xmx6144m"
-                        ,"-Dfile.encoding=UTF-8"
-                        ,"-Duser.country=GB"
-                        ,"-Duser.language=en"
-                        ,"-Duser.variant"
-                        ,"-cp,/home/deepy/.gradle/wrapper/dists/gradle-5.4.1-bin/e75iq110yv9r9wt1a6619x2xm/gradle-5.4.1/lib/gradle-launcher-5.4.1.jar"
-                        ,name
-                        ,"5.4.1");
+                        "/java/bin/java",
+                        "-Xmx6144m",
+                        "-Dfile.encoding=UTF-8",
+                        "-Duser.country=GB",
+                        "-Duser.language=en",
+                        "-Duser.variant",
+                        "-cp,/home/deepy/.gradle/wrapper/dists/gradle-5.4.1-bin/e75iq110yv9r9wt1a6619x2xm/gradle-5.4.1/lib/gradle-launcher-5.4.1.jar",
+                        name,
+                        "5.4.1");
             }
 
             @Override
